46454bccb3f4844b7965d12e622f98eeac9ac560
Yohan Gregorio <yohanggg26@gmail.com>
2024-05-04 03:08:25 -0300

    finalizacao da autenticacao

--- src/main/java/br/com/copadomorro/copadomorro/security/WebSecurityConfig.java
+++ src/main/java/br/com/copadomorro/copadomorro/security/WebSecurityConfig.java
@@ -49,7 +49,6 @@ public class WebSecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/user/**").permitAll()
                         .anyRequest().authenticated());
 
         httpSecurity.addFilterBefore(authFilterToken(), UsernamePasswordAuthenticationFilter.class);
