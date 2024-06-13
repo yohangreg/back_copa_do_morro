576141382d1d794e2a7ed9b9366781eceb4a0385
Yohan Gregorio <yohanggg26@gmail.com>
2024-06-13 16:29:06 -0300

    Minhas mudanças locais no AuthController

--- src/main/java/br/com/copadomorro/backend/controller/AuthController.java
+++ src/main/java/br/com/copadomorro/backend/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/api/auth")
 @CrossOrigin
-public class AuthController {
+public class  AuthController {
 
     @Autowired
     private AuthService authService;
